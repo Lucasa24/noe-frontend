d875d37a80bd396d066584d600d4a8962419d831
arca <lucasalvarezempresa@gmail.com>
2026-09-25 01:47:29 -0300

    Support legacy Academy Pass runtime ID

--- browser-read-any-site-extension/api/extension-config.js
+++ browser-read-any-site-extension/api/extension-config.js
@@ -7,7 +7,8 @@ const BROWSER_READ_RUNTIME_IDS = new Set([
   "nfnpblbakohfcnkngbimljiehklmdcmk",
   "aachjpoooepljhlphhaplfijppgbjdfp",
   "hbokpkaoocpcecbfgfadoplblcfannke",
-  "papoapfhfciiaaadmmondbdkfhgilbki"
+  "papoapfhfciiaaadmmondbdkfhgilbki",
+  "njnehniaiehecdplafcbkdhhmjjcojfe"
 ]);
 
 const RAONY_EMAIL = "raony-oliveira@hotmail.com";
